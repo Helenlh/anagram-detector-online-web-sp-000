a30a045c14fcb61e0aff9e1e3cdf507b19a201bb
HELEN HARMAN <hlharman@bellsouth.net>
2021-01-29 23:56:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,17 +10,7 @@ class Anagram
   def match(words)
     words.select do |word|
     word.split("").sort == @words.split("").sort
-    
   end 
-   
-   
-   
   end 
 end 
-    
-#%w(foo bar) is a shortcut for ["foo", "bar"]
-# 'hello'.match('xx')         #=> nil
-# 'hello'.match('(.)\1')      #=> #<MatchData "ll" 1:"l">
-#/y/.match('haystack') #=> #<MatchData "y">
-#hello world zombies pants dipper'.match('diaper')
 
